bddd6d17589e492094725ee4f4ff5391ea44b4e6
JustaSqu1d <just.a.squid@squ1d.dev>
2024-10-13 15:23:06 -0700

    fix: file paths

--- celesteela-bot/data.py
+++ celesteela-bot/data.py
@@ -1,29 +1,36 @@
-import aiohttp
 import asyncio
-import aiofiles
+import os.path
 from json import loads, dumps
+
+import aiofiles
+import aiohttp
 
 PVPOKE_DATA_URL = "https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/gamemaster/pokemon.json"
 
 MOVES_DATA_URL = "https://raw.githubusercontent.com/JustaSqu1d/EternaCalc/refs/heads/master/pokemon/game_data/moves.json"
+
+filepath = os.path.dirname(__file__)
+
 
 async def fetch_pvpoke_data() -> dict:
     async with aiohttp.ClientSession() as session:
         async with session.get(PVPOKE_DATA_URL) as response:
             return loads(await response.text())
 
+
 async def fetch_moves_data() -> dict:
     async with aiohttp.ClientSession() as session:
         async with session.get(MOVES_DATA_URL) as response:
             return loads(await response.text())
 
+
 async def main():
     pvpoke_data = await fetch_pvpoke_data()
-    async with aiofiles.open("gamedata/pokemon.json", "w") as file:
+    async with aiofiles.open(filepath + "gamedata/pokemon.json", "w") as file:
         await file.write(dumps(pvpoke_data, indent=4))
 
     moves_data = await fetch_moves_data()
-    async with aiofiles.open("gamedata/moves.json", "w") as file:
+    async with aiofiles.open(filepath + "gamedata/moves.json", "w") as file:
         await file.write(dumps(moves_data, indent=4))
 
 
